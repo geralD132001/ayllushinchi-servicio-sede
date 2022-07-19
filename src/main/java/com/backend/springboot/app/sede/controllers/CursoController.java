c4c4feaaa0106979c95fd0cc9ca6d7146e51bb02
GeraldCode <82231832+geralD132001@users.noreply.github.com>
2022-07-19 06:43:37 -0500

    Update CursoController.java

--- src/main/java/com/backend/springboot/app/sede/controllers/CursoController.java
+++ src/main/java/com/backend/springboot/app/sede/controllers/CursoController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.HashMap;
 
-@CrossOrigin(origins = { "http://localhost:4200" })
+@CrossOrigin(origins = { "https://ayllusinchi-app.azurewebsites.net" })
 @RestController
 public class CursoController {
 
